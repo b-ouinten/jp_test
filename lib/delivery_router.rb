f6776dba6d8737ee8d5ff4153f99637c5c195220
ouin85 <bouinten@yahoo.fr>
2021-08-04 08:53:39 +0200

    small change

--- lib/delivery_router.rb
+++ lib/delivery_router.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
 class DeliveryRouter
-  def initialize; end
+  def initialize(restaurants, customers, riders)
+    @actors = [] << restaurants << customers << riders
+    @orders = []
+  end
+
+  def add_order(customer:, restaurant:)
+    @orders << { customer_id: customer, restaurant_id: restraurant }
+  end
+
+  def clear_orders(customer:)
+    @orders.delete_if { |order| order[:customer_id] == customer }
+  end
 end
